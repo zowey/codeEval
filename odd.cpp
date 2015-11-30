d9af514ff490f468438796506f2ce99ae97b89e0
DuoPegla <josip.sarlija@gmail.com>
2015-11-30 22:37:27 +0100

    odd and primes added

---
+++ odd.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    
+    for (int i=1; i<100; ++i)
+    {
+        if (i % 2 != 0)
+            cout << i << endl;
+    }
+    return 0;
+}
--- primes.cpp
+++ primes.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
+#include <cmath>
+using namespace std;
 
-int main()
+bool isPrime(int n)
 {
-    
-    for (int i=1; i<100; ++i){
-        
-        
+    if (n < 2)
+    {
+        return false;
     }
     
+    for (int i=2; i<=sqrt(n); ++i)
+        if (n % i == 0)
+            return false;
     
-
-    return 0;
+    return true;
 }
 
-void isPrime(int n)
-{
-
+int main(int argc, char *argv[]) {
+    
+    for (int i=1; i<100; ++i)
+    {
+        if (isPrime(i) == true)
+            cout << i << endl;
+    }
+    return 0;
 }
